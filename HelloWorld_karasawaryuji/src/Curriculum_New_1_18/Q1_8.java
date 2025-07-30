a33d95225ca20b8b8187e0cfc95ede383b963bbc
karasawaryuji <karasawaryuji@localhost>
2025-07-30 21:07:24 +0900

    currculumB

--- HelloWorld_karasawaryuji/src/Curriculum_New_1_18/Q1_8.java
+++ HelloWorld_karasawaryuji/src/Curriculum_New_1_18/Q1_8.java
@@ -7,17 +7,28 @@ public class Q1_8 {
 		/*
 		 * Q1：引数に文字列型と整数型をいれてコンソールに「Hello JavaSE 11」と出力するメソッドを作成してください。
 		 */
-		
-		msg("Hello JavaSE");
-		msg("11");
 
-		  public static void test1 (String a int b) {
-		        System.out.println("Hello JavaSE 11");
-		    }
+		msg("Hello JavaSE", 11);
+		msg2(1, 2);
+		test(1);
 	}
 
+	public static void msg(String a, int b) {
+		System.out.println("Hello JavaSE 11");
+	}
 	/*
 	 * Q2引数に整数を渡すと渡した値同士を乗算しコンソールに出力するメソッドを作成してください
 	 */
+
+	public static void msg2(int num1, int num2) {
+		System.out.println(num1 * num2);
+	}
+
+	/*
+	 * Q3：引数として整数の配列を渡すと、受け取った値を順番にコンソールに出力するメソッドを作成
+	 */
+	private static void test(int[] nums) {
+		System.out.println();
+	}
 
 }
--- HelloWorld_karasawaryuji/src/curriculumB/Ques7.java
+++ HelloWorld_karasawaryuji/src/curriculumB/Ques7.java
@@ -1,87 +1,117 @@
 package curriculumB;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ques7 {
+
 	public static void main(String[] args) {
 
 		/*
-		 * N人の生徒の成績を管理するプログラムを下記条件で作成してください
+		 * 【概要】N人の生徒の成績を入力し管理する処理
+		 * 【詳細】N人の生徒の成績を入力し、各生徒の科目平均点、全体の科目平均点、各教科の平均点を出力する
 		 */
+
+		// Scannerクラスのインスタンスを作成
+		// 引数で標準入力System.inを指定する
 		Scanner scanner = new Scanner(System.in);
-		//String name = "";
-		System.out.print("生徒の人数を入力してください(2以上) :");
-		int num = scanner.nextInt();
-		
 
-		ArrayList<ArrayList<Integer>> arrays = new ArrayList<ArrayList<Integer>>();
-		for (int i = 1; i <= num; i++) {
-			ArrayList<Integer> list = new ArrayList<>();
-			System.out.print(i + "人目の『英語』の点数を入力してください :");
-			list.add(scanner.nextInt());
-			System.out.print(i + "人目の『数学』の点数を入力してください :");
-			list.add(scanner.nextInt());
-			System.out.print(i + "人目の『理科』の点数を入力してください :");
-			list.add(scanner.nextInt());
-			System.out.print(i + "人目の『社会』の点数を入力してください :");
-			list.add(scanner.nextInt());
-			arrays.add(list);
-			System.out.println();
-			
-		}
+		// 「生徒の人数を入力してください(2以上):」と出力する
+		System.out.print("生徒の人数を入力してください(2以上):");
+		// 変数を宣言し数値を返す
+		int students = scanner.nextInt();
 
-		double ave;
-		int sum = 0;
-		double aveE;
-		int sumE = 0;
-		double aveS;
-		int sumS = 0;
-		double aveR;
-		int sumR = 0;
-		double aveSh;
-		int sumSh = 0;
-		
-		
+		// 科目を格納する配列を作成
+		String[] subjects = { "英語", "数学", "理科", "社会" };
 
+		// 多次元配列を作成
+		int[][] scores;
+		scores = new int[students][subjects.length];
 
-		for (int i = 1; i <= num; i++) {
-			for (Integer s : arrays.get(i-1)) {
-				sum = sum + s;
+		for (int i = 0; i < students; i++) {
+
+			for (int j = 0; j < subjects.length; j++) {
+
+				// 点数の入力欄を出力する
+				System.out.print((i + 1) + "人目の" + subjects[j] + "の点数を入力してください:");
+				// 変数を宣言し数値を返す
+				int score = scanner.nextInt();
+				// 配列に変数の数値を格納
+				scores[i][j] = score;
 			}
-			ave = (double) sum / 4;
-
-			System.out.println(i + "人目の平均点は" + String.format("%.2f",ave) + "です。");
-			
-			sum = 0;
-			ave=0;
-			
-		sumE = sumE + arrays.get(i-1).get(0);
-		sumS = sumS + arrays.get(i-1).get(1);
-		sumR = sumR + arrays.get(i-1).get(2);
-		sumSh = sumSh+ arrays.get(i-1).get(3);
-		
-		
+			// 改行する
+			System.out.println();
 		}
+
+		// 実行後に閉じる
+		scanner.close();
+
+		for (int i = 0; i < students; i++) {
+			// 生徒の合計点を表す変数を作成
+			double stuSum = 0d;
+			// 生徒の平均点を表す変数を作成
+			double stuAv = 0d;
+
+			for (int j = 0; j < subjects.length; j++) {
+
+				// 生徒ごとの合計点を算出する計算式を作成
+				stuSum += scores[i][j];
+
+			}
+			// 生徒ごとの平均点を算出する計算式を作成
+			stuAv = stuSum / subjects.length;
+			// 変数を宣言し平均点を小数第2位までの表示にして格納
+			String studentAv = String.format("%.2f", stuAv);
+			// 生徒ごとの平均点を出力する
+			System.out.println((i + 1) + "人目の平均点は" + studentAv + "点です。");
+		}
+
+		// 改行する
 		System.out.println();
-		
-		aveE = (double) sumE/ num ;
-		System.out.println("英語の平均点は" +  String.format("%.2f",aveE) + "です。");
-		
-		aveS = (double) sumS/ num ;
-		System.out.println( "数学の平均点は" +  String.format("%.2f",aveS) + "です。");
-		
-		aveR = (double) sumR/ num ;
-		System.out.println("理科の平均点は" +  String.format("%.2f",aveR) + "です。");
-		
-		aveSh = (double) sumSh/ num ;
-		System.out.println("社会の平均点は" +  String.format("%.2f",aveSh) + "です。");
-		
-		double aveAll =  (double)(sumE + sumS + sumR + sumSh) / (num * 4);
-		
-		System.out.println("全体の平均点は" +  String.format("%.2f",aveAll) + "です。");
-		
-	
+
+		// 配列を作成
+		double[] allAv;
+		allAv = new double[subjects.length];
+
+		for (int i = 0; i < subjects.length; i++) {
+			// 科目の合計点を表す変数を作成
+			double subSum = 0d;
+			// 科目の平均点を表す変数を作成
+			double subAv = 0d;
+
+			for (int j = 0; j < students; j++) {
+
+				// 科目ごとの合計点を算出する計算式を作成
+				subSum += scores[j][i];
+
+			}
+			// 科目ごとの平均点を算出する計算式を作成
+			subAv = subSum / students;
+			// 変数を宣言し平均点を小数第2位までの表示にして格納
+			String subjectAv = String.format("%.2f", subAv);
+			// 科目ごとの平均点を出力する
+			System.out.println(subjects[i] + "の平均点は" + subjectAv + "点です。");
+
+			// 配列に変数の数値を格納
+			allAv[i] = subAv;
+		}
+
+		// 変数を宣言し初期化する
+		double total = 0;
+
+		for (int i = 0; i < allAv.length; i++) {
+
+			// 科目ごとの平均点を格納した配列の要素を合計する
+			total += allAv[i];
+
+		}
+
+		// 全体の平均を求める式を作成
+		double all = total / allAv.length;
+		// 変数を宣言し平均点を小数第2位までの表示にして格納
+		String avarage = String.format("%.2f", all);
+		// 全体の平均点を出力する
+		System.out.println("全体の平均点は" + avarage + "点です。");
 
 	}
+
 }
